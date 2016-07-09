2a0f94285ae11d043533ccb2efd4e9e15748db45
unknown <>
2016-07-09 18:32:15 +0800

    上一個版本有錯誤，已經修正了

--- app/src/main/java/itac/together/MainActivity.java
+++ app/src/main/java/itac/together/MainActivity.java
@@ -1,6 +1,7 @@
 package itac.together;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,14 +25,12 @@ import android.widget.RelativeLayout;
 public class MainActivity extends AppCompatActivity {
 
 
-    RelativeLayout Main_RL;
+    RelativeLayout Main_RelaLay, OutestRelLay;
     View androidSupDsignCooLay;
     View side;
     ViewGroup Parent;
     boolean sideHasShown = false;
     LayoutInflater LayInflater;
-
-    ImageButton []sideButtons;
 
 
     @Override
@@ -41,25 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
         Proclaim_view();//MVC模式的宣告函式
 
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//很重要 要讓2.X的使用就要處理好
-
-        /*  目前不需要用吧? 不知道為什麼有這個?? Can anyone answer me this question?
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
 
 
         //每當Relative Layout 被點下去的時候就會進行偵測
-        Main_RL.setOnTouchListener(new View.OnTouchListener() {
+        Main_RelaLay.setOnTouchListener(new View.OnTouchListener() {
             // 建立GestureDetector物件，並傳入自己定義的手勢物件（繼承自SimpleOnGestureListener）
             GestureDetector gd = new GestureDetector(MainActivity.this, new SideGestureDetectorListener());
 
@@ -70,13 +57,12 @@ public class MainActivity extends AppCompatActivity {
                 //判斷是不是已經顯示側邊滑動欄(side)了
                 if(sideHasShown)
                 {
-                    Parent.addView(side);
-
-                    side.setOnClickListener(sideGoBack);
                 }
                 else
                 {
                     //還沒出現側邊滑動欄(side)的話就用手勢偵測去判斷要不要顯示側邊滑動欄(side)
+                    Log.d("get into ontouch event", "");
+
                     gd.onTouchEvent(event);
                 }
                 return true; // 必須回傳true,因為已經了
@@ -93,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -118,74 +103,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
     public void  Proclaim_view()
     {
-        Main_RL = (RelativeLayout) findViewById(R.id.Main_RL);
+        Main_RelaLay = (RelativeLayout) findViewById(R.id.Main_RelaLay);
         androidSupDsignCooLay = findViewById(R.id.androidSupDsignCooLay);
+        OutestRelLay = (RelativeLayout)findViewById(R.id.OutestRelLay);
         //Parent 是比看的到的元件還要外面的那一層  加View 在這底下就可以蓋過Action Bar 了~~(查了好久Q_Q)
-        Parent = (ViewGroup) androidSupDsignCooLay.getParent();
+
+
+
+
+        Parent = (ViewGroup) OutestRelLay.getParent();
         //把滑動的側邊直接轉成View 等待呼叫  不然不知道使用著會呼叫到多少次 = =
-        LayInflater = getLayoutInflater();
-        side = LayInflater.inflate(R.layout.side_mode_select, null);
+
+
+        LayInflater =  (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        side = LayInflater.inflate(R.layout.side_menu, null);
+        Parent.addView(side);
+        side.setVisibility(View.GONE);
+        Parent.bringToFront();
+        Log.d("Layout Inflater 已經做好了", "side已經放進去了");
     }
 
 
-    private View.OnClickListener sideGoBack = new View.OnClickListener() {
+
+
+    private View.OnTouchListener sideGoBack = new View.OnTouchListener() {
         @Override
-        public void onClick(View v) {
-            if(v.getX() >240)
+        public boolean onTouch(View v, MotionEvent event) {
+            if(event.getX() >120)
             {
                 sideHasShown = false;
-                Parent.removeView(side);
+
+                side.setVisibility(View.GONE);
+
+                return true;
             }
+            else
+                return false;
         }
     };
 
 
-
     class SideGestureDetectorListener extends GestureDetector.SimpleOnGestureListener
     {
-
-        @Override
-        public void onLongPress(MotionEvent e)
-        {
-            Log.d("MyGestureDetector", "onLongPress");
-            super.onLongPress(e);
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-                                float distanceY)
-        {
-            /*Log.d("MyGestureDetector", "onScroll");
-
-            DisplayMetrics metrics = new DisplayMetrics();
-            getWindowManager().getDefaultDisplay().getMetrics(metrics);//取得螢幕大小
-
-
-
-            if(e1.getX() < metrics.widthPixels / 2 && distanceX >= metrics.widthPixels/3)
-            {
-
-                LayoutInflater LInflater = getLayoutInflater();
-                                LayoutInflater LIf = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                                View v = LIf.inflate(R.layout.side_mode_select, Main_RL, true);
-                //v.setVisibility(View.VISIBLE);
-
-
-                Log.d("In On Scroll IFFFF", "return True");
-                return super.onScroll(e1, e2, distanceX, distanceY);
-            }
-            else {
-
-                Log.d("In On Scroll IFFFF", "return False");
-                return false;
-            }*/
-            return true;
-        }
-
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
                                float velocityY) {
@@ -195,10 +156,12 @@ public class MainActivity extends AppCompatActivity {
 
             if(e1.getX() < metrics.widthPixels / 2  && velocityX >= 1)
             {
-                /*LayoutInflater LIf = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                                View v = LayoutInflater.inflate(R.layout.side_mode_select, Main_RL);*/
 
                 sideHasShown = true;
+                side.setVisibility(View.VISIBLE);
+                side.setOnTouchListener(sideGoBack);
+
+
                 Log.d("In On Fling IFFFF", "return True");
                 return super.onFling(e1, e2, velocityX, velocityY);
             }
